955cb036403fd632d4e8ca8d0a0bb1cebefd725d
Cristo <crisconru@gmail.com>
2018-04-05 17:44:22 +0200

    Creado protocolo con hex y con dec

--- src/escpos.py
+++ src/escpos.py
@@ -1,5 +1,5 @@
 # Non ASCII characters
-esc = {
+esc_hex = {
     'NUL': '00',
     'SOH': '01',
     'STX': '02',
@@ -36,17 +36,69 @@ esc = {
     'DEL': '7F'
 }
 
+esc_dec = {
+    'NUL': 0,
+    'SOH': 1,
+    'STX': 2,
+    'ETX': 3,
+    'EOT': 4,
+    'ENQ': 5,
+    'ACK': 6,
+    'BEL': 7,
+    'BS': 8,
+    'HT': 9,
+    'LF': 10,
+    'VT': 11,
+    'FF': 12,
+    'CR': 13,
+    'SO': 14,
+    'SI': 15,
+    'DLE': 16,
+    'DC1': 17,
+    'DC2': 18,
+    'DC3': 19,
+    'DC4': 20,
+    'NAK': 21,
+    'SYN': 22,
+    'ETB': 23,
+    'CAN': 24,
+    'EM': 25,
+    'SUB': 26,
+    'ESC': 27,
+    'FS': 28,
+    'GS': 29,
+    'RS': 30,
+    'US': 31,
+    'SP': 32,
+    'DEL': 127
+}
 
-def esc_command(*args):
+
+def esc_command_hex(*args):
     msg = bytearray()
     for i in args:
         aux = None
-        if i in esc:
-            aux = bytearray.fromhex(esc[i])
+        if i in esc_hex:
+            aux = bytearray.fromhex(esc_hex[i])
         elif isinstance(i, str):
             aux = i.encode('ascii')
         elif isinstance(i, int):
-            aux = chr(i)
+            aux = bytes(chr(i), 'ascii')
+        if aux is not None:
+            msg.extend(aux)
+    return msg
+
+
+def esc_command_dec(*args):
+    msg = bytearray()
+    for i in args:
+        aux = None
+        if i in esc_dec:
+            aux = bytes(chr(esc_dec[i]), 'utf-8')
+        elif isinstance(i, str):
+            aux = i.encode('utf-8')
+        elif isinstance(i, int):
+            aux = bytes(chr(i), 'utf-8')
         if aux is not None:
             msg.extend(aux)
     return msg
@@ -54,8 +106,8 @@ def esc_command(*args):
 
 if __name__ == '__main__':
     print('Inicializacion -> ESC @')
-    print(esc_command('ESC', '@'))
+    print(esc_command_hex('ESC', '@'))
     print('Mando texto -> Hola mundo')
-    print(esc_command('Hola mundo'))
+    print(esc_command_hex('Hola mundo'))
     print('Imprimir linea -> LF')
-    print(esc_command('LF'))
+    print(esc_command_hex('LF'))
--- test/test_escpos.py
+++ test/test_escpos.py
@@ -1,16 +1,31 @@
 from src import escpos
 
 
-def test_esc():
+def test_esc_hex():
     esc = b'\x1b'
-    assert escpos.esc_command('ESC') == esc
+    assert escpos.esc_command_hex('ESC') == esc
 
 
-def test_esc_initialization():
+def test_esc_hex_initialization():
     esc = b'\x1b@'
-    assert escpos.esc_command('ESC', '@') == esc
+    assert escpos.esc_command_hex('ESC', '@') == esc
 
 
-def test_esc_text():
+def test_esc_hex_text():
     esc = 'Hello world'.encode('utf-8')
-    assert escpos.esc_command('Hello world') == esc
+    assert escpos.esc_command_hex('Hello world') == esc
+
+
+def test_esc_dec():
+    esc = b'\x1b'
+    assert escpos.esc_command_dec('ESC') == esc
+
+
+def test_esc_dec_initialization():
+    esc = b'\x1b@'
+    assert escpos.esc_command_dec('ESC', '@') == esc
+
+
+def test_esc_dec_text():
+    esc = 'Hello world'.encode('utf-8')
+    assert escpos.esc_command_dec('Hello world') == esc
