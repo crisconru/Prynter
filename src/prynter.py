b18faf60a35b5eacea4fd77db398d73f56c3310e
Cristo <crisconru@gmail.com>
2018-04-18 18:38:12 +0200

    Rula la Adafruit y la STP80, pero no la APPROX AAPOS80AM ni la EPSON TM-T20II

--- src/prynter.py
+++ src/prynter.py
@@ -1,4 +1,4 @@
-from serial import Serial
+from serial import Serial, EIGHTBITS, PARITY_NONE, STOPBITS_ONE
 from serial.tools.list_ports import comports
 from escpos import ESCPOS
 
@@ -18,20 +18,29 @@ def select_device():
 
 class Prynter(Serial):
     def __init__(self, port=None, printer='stp80'):
-        super().__init__()
+        super().__init__(port=port, baudrate=38400, bytesize=EIGHTBITS,
+                         parity=PARITY_NONE, stopbits=STOPBITS_ONE,
+                         dsrdtr=True)
         self.printer = printer
         self.port = port
+        '''
         if printer == 'stp80':
+            print('stp80')
             self.baudrate = 9600
             self.dsrdtr = True
         elif printer == 'adafruit':
+            print('adafruit')
             self.baudrate = 9600
-        else:
-            self.baudrate = 9600
+        elif printer == 'epson':
+            print('epson')
+            self.baudrate = 38400
+            self.bytesize = EIGHTBITS
+            self.parity = PARITY_NONE
+            self.stopbits = STOPBITS_ONE
             self.dsrdtr = True
             self.rtscts = False
-            self.xonxoff = False
-        self.open()
+            self.xonxoff = None
+        '''
         print('Comunicacion abierta = {}'.format(self.is_open))
         self.esc = ESCPOS()
         self.esc.encoding('ascii')
@@ -46,12 +55,21 @@ class Prynter(Serial):
                 self.open()
                 self.send(msg, rep-1)
 
+    def receive(self):
+        self.timeout = 5.0
+        rx = self.readline().decode()
+        print('recibido = {}'.format(rx))
+
     def initialization(self):
         msg = self.esc.ascii_command('ESC', '@')
         if self.send(msg):
             print('Initialization send')
             if self.printer == 'adafruit':
                 self.adafruit()
+            #elif self.printer != 'stp80':
+            #    msg = self.esc.ascii_command('ESC', 'S')
+            #    if self.send(msg):
+            #        print('Inicializacion de impresora approx')
 
     def adafruit(self):
             msg = self.esc.ascii_command('ESC', '7', 7, 80, 2)
@@ -91,26 +109,42 @@ class Prynter(Serial):
             print('Cut send')
 
     def test(self):
+        #self.receive()
         self.justify('C')
+        #self.receive()
         self.print('Centro')
+        #self.receive()
         self.justify('R')
+        #self.receive()
         self.bold_text(1)
+        #self.receive()
         self.print('Derecha negra')
+        #self.receive()
         self.justify()
+        #self.receive()
         self.bold_text(0)
+        #self.receive()
         self.print('Izquierda normal')
+        #self.receive()
+        self.esc.encoding('utf-8')
         self.print('Como no estás experimentado en las cosas del mundo, todas '
                    'las cosas que tienen algo de dificultad te parecen '
                    'imposibles. Confía en el tiempo, que suele dar dulces '
-                   'a muchas amargas dificultades.')
-        self.line_feed()
-        self.cut(partial=False)
+                   'salidas a muchas amargas dificultades.')
+        #self.receive()
+        #self.line_feed(6)
+        #self.receive()
+        #msg = self.esc.ascii_command('FF')
+        #self.send(msg)
+        #self.receive()
+        self.cut(partial=True)
+        #self.receive()
 
 
 if __name__ == '__main__':
     port = select_device()
     print('Seleccionado {}'.format(port))
-    impresora = Prynter(port=port, printer='otra')
+    impresora = Prynter(port=port, printer='epson')
     opt = '9'
     while opt != '0':
         txt = input('0) Salir\n1) Inicializar\n2) Hola mundo\n3) Line Feed\n'
